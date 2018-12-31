fbc2fab7aef8a8ce32964b4e26786457dd468ce0
Keshav <kgupt1@ocdsb.ca>
2018-12-31 14:46:39 -0500

    removed the majority of the white border

--- home.py
+++ home.py
@@ -18,9 +18,10 @@ class ButtonsDemo:
         PTButton = PhotoImage(file = "Images/PTButton.gif")
         HTButton = PhotoImage(file = "Images/HTButton.gif")
 
-        Button(image = GSButton, command = self.GenSafetyFunc).place(x=260, y=600, anchor = CENTER)
-        Button(image = PTButton, command = self.PowToolSafetyFunc).place(x=500, y=600, anchor = CENTER)
-        Button(image = HTButton, command = self.HanToolSafetyFunc).place(x=750, y=600, anchor = CENTER)
+        Button(image = GSButton, command = self.GenSafetyFunc, highlightbackground="#38761d", borderwidth=0).place(x=260, y=600, anchor = CENTER)
+        Button(image = PTButton, command = self.PowToolSafetyFunc, highlightbackground="#38761d", borderwidth=0).place(x=500, y=600, anchor = CENTER)
+        Button(image = HTButton, command = self.HanToolSafetyFunc, highlightbackground="#38761d", borderwidth=0).place(x=750, y=600, anchor = CENTER)
+
         window.mainloop()
 
     def GenSafetyFunc(self):
