2bedabaccbde13e35a5c29fed6e35de6d3ea98dc
J C <>
2019-01-02 12:53:55 -0500

    saving to txt - JC

--- GenResult.py
+++ GenResult.py
@@ -4,6 +4,7 @@ if version_info.major == 2:
 elif version_info.major == 3:
     from tkinter import *
 
+saves=open("saves","w")
 
 def ReturnMark(ans):
     correct = 0
@@ -20,7 +21,14 @@ def ReturnMark(ans):
         if ans[iteration] == True:
             correct += 1
     Label(canvas,text="You got {} out of 10 correct".format(correct),font=("Helvetica",20),bg="#38761d",fg="white").place(x=500,y=200, anchor=CENTER)
-    percent=Label(canvas,text="{}{}".format((correct/10)*100,"%"),font=("Helvetica",60),fg="white",bg="#38761d")
+
+    mark=(correct/10)*100
+    percent=Label(canvas,text="{}{}".format(mark,"%"),font=("Helvetica",60),fg="white",bg="#38761d")
     percent.place(x=400,y=350)
     print(ans)
     ResWindow.mainloop()
+    saves.apped(mark)
+    saves.close()
+    saves.open("saves","r")
+    saves.read()
+    saves.close()
