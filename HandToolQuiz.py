bde76fc6c26c90fcd3f4cb091ed5d374504c15de
J C <>
2019-01-04 12:18:43 -0500

    fixing drop boxes in hand tools -JC

--- HandToolQuiz.py
+++ HandToolQuiz.py
@@ -49,13 +49,30 @@ def HTQuiz():
                 HTcanvas.create_text(choicePos[0][choices.index(choices[iteration])],choicePos[1][choices.index(choices[iteration])],text=choices[choices.index(choices[iteration])],font=('Helvetica', 15),fill="white")
         op1=HTcanvas.create_text(choicePos[0][choices.index(selected)],choicePos[1][choices.index(selected)],text=choices[choices.index(selected)],font=('Helvetica', 15,"overstrike"),fill="red")
 
+    dbPos=[[],[]]
+    def boxPositioning(x,y,text,bDB):
+        space=5
+        qxPos=x
+        qyPos=y
+        q1="Q1: Minor injuries do not need to be reported. -> "
+        used=False
+
+        for word in text.split(" "):
+
+            question=HTcanvas.create_text(qxPos,qyPos,text=word, anchor = "nw", font=('Helvetica', 15), fill="white")
+            qbox=HTcanvas.bbox(question)
+            qxPos=qbox[2]+space
+
+            if (str(word) == str(bDB) and used==False):
+                dbPos[0]=qbox[2]+space
+                dbPos[1]=qbox[1]
+                used=True
 
     #QUESTION 1---
-    text_canvas = HTcanvas.create_text(25, 325, anchor = "nw", font=('Helvetica', 15), fill="white")
-    HTcanvas.itemconfig(text_canvas, text="Q1: You should use a long throat clamp instead of a deep throat clamp. ->")
+    boxPositioning(25, 325,"Q1: You should use a long throat clamp instead of a deep throat clamp. ->","->")
     WordChoice1 = StringVar(HTWindow)
     WordChoice1.set('Answer Here          ')
-    WordDropdown1 = OptionMenu(HTcanvas, WordChoice1, *choices).place(x=510,y=320)
+    WordDropdown1 = OptionMenu(HTcanvas, WordChoice1, *choices).place(x=dbPos[0],y=dbPos[1])
     def change_dropdown1(*args):
         strike(WordChoice1.get())
         if WordChoice1.get() == "False" or WordChoice1.get() == "No":
@@ -66,11 +83,10 @@ def HTQuiz():
     WordChoice1.trace('w', change_dropdown1)
 
     #QUESTION 2---
-    text_canvas = HTcanvas.create_text(25, 365, anchor = "nw", font=('Helvetica', 15), fill="white")
-    HTcanvas.itemconfig(text_canvas, text="Q2: Without padding, a clamp can leave marks on your work ->")
+    boxPositioning(25, 365,"Q2: Without padding, a clamp can leave marks on your work ->","->")
     WordChoice2 = StringVar(HTWindow)
     WordChoice2.set('Answer Here          ')
-    WordDropdown2 = OptionMenu(HTcanvas, WordChoice2, *choices).place(x=450,y=360)
+    WordDropdown2 = OptionMenu(HTcanvas, WordChoice2, *choices).place(x=dbPos[0],y=dbPos[1])
     def change_dropdown2(*args):
         strike(WordChoice2.get())
         if WordChoice2.get()=="Yes" or WordChoice2.get()=="True":
@@ -80,11 +96,10 @@ def HTQuiz():
     WordChoice2.trace('w', change_dropdown2)
 
     #QUESTION 3---
-    text_canvas = HTcanvas.create_text(25, 405, anchor = "nw", font=('Helvetica', 15), fill="white")
-    HTcanvas.itemconfig(text_canvas, text="Q3: We store C clamps in the                                           .")
+    boxPositioning(25, 405,"Q3: We store C clamps in the                          .","the")
     WordChoice3 = StringVar(HTWindow)
     WordChoice3.set('Answer Here          ')
-    WordDropdown3 = OptionMenu(HTcanvas, WordChoice3, *choices).place(x=230,y=400)
+    WordDropdown3 = OptionMenu(HTcanvas, WordChoice3, *choices).place(x=dbPos[0],y=dbPos[1])
     def change_dropdown3(*args):
         strike(WordChoice3.get())
         if WordChoice3.get()=="Drawer":
@@ -94,11 +109,10 @@ def HTQuiz():
     WordChoice3.trace('w', change_dropdown3)
 
     #QUESTION 4---
-    text_canvas = HTcanvas.create_text(25, 445, anchor = "nw", font=('Helvetica', 15), fill="white")
-    HTcanvas.itemconfig(text_canvas, text="Q4: For hammers, you should use the                                                 to hit an object")
+    boxPositioning(25, 445,"Q4: For hammers, you should use the                          to hit an object","the")
     WordChoice4 = StringVar(HTWindow)
     WordChoice4.set('Answer Here          ')
-    WordDropdown4 = OptionMenu(HTcanvas, WordChoice4, *choices).place(x=275,y=440)
+    WordDropdown4 = OptionMenu(HTcanvas, WordChoice4, *choices).place(x=dbPos[0],y=dbPos[1])
     def change_dropdown4(*args):
         strike(WordChoice4.get())
         if WordChoice4.get()=="Face":
@@ -108,11 +122,10 @@ def HTQuiz():
     WordChoice4.trace('w', change_dropdown4)
 
     #QUESTION 5---
-    text_canvas = HTcanvas.create_text(25, 485, anchor = "nw", font=('Helvetica', 15), fill="white")
-    HTcanvas.itemconfig(text_canvas, text="Q5: For a hammer, You should avoid                                                 blows when hitting an object.")
+    boxPositioning(25, 485,"Q5: For a hammer, You should avoid                          blows when hitting an object.","avoid")
     WordChoice5 = StringVar(HTWindow)
     WordChoice5.set('Answer Here          ')
-    WordDropdown5 = OptionMenu(HTcanvas, WordChoice5, *choices).place(x=265,y=480)
+    WordDropdown5 = OptionMenu(HTcanvas, WordChoice5, *choices).place(x=dbPos[0],y=dbPos[1])
     def change_dropdown5(*args):
         strike(WordChoice5.get())
         if WordChoice5.get()=="Glancing":
@@ -122,11 +135,10 @@ def HTQuiz():
     WordChoice5.trace('w', change_dropdown5)
 
     #QUESTION 6---
-    text_canvas = HTcanvas.create_text(25, 525, anchor = "nw", font=('Helvetica', 15), fill="white")
-    HTcanvas.itemconfig(text_canvas, text="Q6: The square head screwdriver is called a:")
+    boxPositioning(25, 525,"Q6: The square head screwdriver is called a:","a:")
     WordChoice6 = StringVar(HTWindow)
     WordChoice6.set('Answer Here          ')
-    WordDropdown6 = OptionMenu(HTcanvas, WordChoice6, *choices).place(x=320,y=520)
+    WordDropdown6 = OptionMenu(HTcanvas, WordChoice6, *choices).place(x=dbPos[0],y=dbPos[1])
     def change_dropdown6(*args):
         strike(WordChoice6.get())
         if WordChoice6.get()=="Robertson":
@@ -136,11 +148,10 @@ def HTQuiz():
     WordChoice6.trace('w', change_dropdown6)
 
     #QUESTION 7---
-    text_canvas = HTcanvas.create_text(25, 565, anchor = "nw", font=('Helvetica', 15), fill="white")
-    HTcanvas.itemconfig(text_canvas, text="Q7: You should use an                                                 in tight areas")
+    boxPositioning(25, 565,"Q7: You should use an                          in tight areas","an")
     WordChoice7 = StringVar(HTWindow)
     WordChoice7.set('Answer Here          ')
-    WordDropdown7 = OptionMenu(HTcanvas, WordChoice7, *choices).place(x=177,y=560)
+    WordDropdown7 = OptionMenu(HTcanvas, WordChoice7, *choices).place(x=dbPos[0],y=dbPos[1])
     def change_dropdown7(*args):
         strike(WordChoice7.get())
         if WordChoice7.get()=="Offset Screwdriver":
@@ -150,11 +161,10 @@ def HTQuiz():
     WordChoice7.trace('w', change_dropdown7)
 
     #QUESTION 8---
-    text_canvas = HTcanvas.create_text(25, 605, anchor = "nw", font=('Helvetica', 15), fill="white")
-    HTcanvas.itemconfig(text_canvas, text="Q8: You should use a                                                 blade when cutting")
+    boxPositioning(25, 605,"Q8: You should use a                          blade when cutting","a")
     WordChoice8 = StringVar(HTWindow)
     WordChoice8.set('Answer Here          ')
-    WordDropdown8 = OptionMenu(HTcanvas, WordChoice8, *choices).place(x=175,y=600)
+    WordDropdown8 = OptionMenu(HTcanvas, WordChoice8, *choices).place(x=dbPos[0],y=dbPos[1])
     def change_dropdown8(*args):
         strike(WordChoice8.get())
         if WordChoice8.get()=="Sharp":
@@ -164,11 +174,10 @@ def HTQuiz():
     WordChoice8.trace('w', change_dropdown8)
 
     #QUESTION 9---
-    text_canvas = HTcanvas.create_text(25, 645, anchor = "nw", font=('Helvetica', 15), fill="white")
-    HTcanvas.itemconfig(text_canvas, text="Q9: Do not                                                 a falling knife")
+    boxPositioning(25, 645,"Q9: Do not                          a falling knife","not")
     WordChoice9 = StringVar(HTWindow)
     WordChoice9.set('Answer Here          ')
-    WordDropdown9 = OptionMenu(HTcanvas, WordChoice9, *choices).place(x=100,y=640)
+    WordDropdown9 = OptionMenu(HTcanvas, WordChoice9, *choices).place(x=dbPos[0],y=dbPos[1])
     def change_dropdown9(*args):
         strike(WordChoice9.get())
         if WordChoice9.get()=="Grab":
@@ -178,11 +187,10 @@ def HTQuiz():
     WordChoice9.trace('w', change_dropdown9)
 
     #QUESTION 10---
-    text_canvas = HTcanvas.create_text(25, 685, anchor = "nw", font=('Helvetica', 15), fill="white")
-    HTcanvas.itemconfig(text_canvas, text="Q10: You can leave hot glue guns unattended if unplugged. ->")
+    boxPositioning(25, 685,"Q10: You can leave hot glue guns unattended if unplugged. ->","->")
     WordChoice10 = StringVar(HTWindow)
     WordChoice10.set('Answer Here          ')
-    WordDropdown10 = OptionMenu(HTcanvas, WordChoice10, *choices).place(x=427,y=680)
+    WordDropdown10 = OptionMenu(HTcanvas, WordChoice10, *choices).place(x=dbPos[0],y=dbPos[1])
     def change_dropdown10(*args):
         strike(WordChoice10.get())
         if WordChoice10.get()=="Yes" or WordChoice10.get()=="True":
