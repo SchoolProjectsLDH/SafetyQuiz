df3cbeb7b60f56512afbca5e97f529e0ac73c0ed
J C <>
2019-01-07 22:41:12 -0500

    Randomizing Power Tool Quiz options -JC

--- PowerToolQuiz.py
+++ PowerToolQuiz.py
@@ -6,7 +6,7 @@ elif version_info.major == 3:
     from tkinter import *
 
 def PTQuiz():
-    choices = ["Jewelery","Long","Kickbacks","Angular","Front","Side","Speed","Grab","Pull","Machine","Waste","Lock Out","Break","Bind"]
+    choices = ["Side","Angular","Machine","Grab","Long","Waste","Pull","Bind","Lock Out","Front""Break","Kickbacks","Jewelery","Speed"]
     CheckCorrect = [False,False,False,False,False,False,False,False,False,False,False,False,False,False]
     PTWindow=Tk()
     PTWindow.title("Power Tool Quiz")
