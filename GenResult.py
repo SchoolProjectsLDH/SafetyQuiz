0c945b0379fac54e5af43186f3d0e7f070b6332c
Keshav <kgupt1@ocdsb.ca>
2019-01-08 21:06:49 -0500

    comments

--- GenResult.py
+++ GenResult.py
@@ -4,16 +4,23 @@ if version_info.major == 2:
 elif version_info.major == 3:
     from tkinter import *
 def ReturnMark(ans):
+    #Variables
     backgroundcolour = "#460000"
     correct = 0.0
     saves=open("saves","a")
+
+    #Creating tkinter window
     ResWindow=Tk()
     ResWindow.title("Result")
     ResWindow.geometry("1000x800")
     ResWindow.resizable(False,False)
+
+    #Creating window canvas
     canvas = Canvas(ResWindow, width = 1000, height = 800)
     canvas.create_rectangle(0, 0, 1000, 8000, fill = backgroundcolour)
     canvas.pack(side = "top", fill = "both", expand = True)
+
+    #Window content
     Label(canvas,text="Your Result",font=("Helvetica",30),bg= backgroundcolour,fg="white").place(x=500,y=25,anchor=CENTER)
     for iteration in range(0,len(ans)):
         if ans[iteration] == True:
@@ -23,6 +30,10 @@ def ReturnMark(ans):
     Label(canvas,text="You got {} out of 10 correct".format(int(correct)),font=("Helvetica",20),bg=backgroundcolour,fg="white").place(x=500,y=200, anchor=CENTER)
     Label(canvas,text="{}{}".format(mark,"%"),font=("Helvetica",60),fg="white",bg=backgroundcolour).place(x=500,y=350, anchor = CENTER)
     Label(canvas,text="Open the home window which is minimized below to choose another category.\n Keep this window open for later reference.",font=("Helvetica",15),bg=backgroundcolour,fg="white").place(x=500,y=700,anchor=CENTER)
+
+    #Saving to file
     saves.write("General Safety Quiz: {}".format(text) + "\n")
     saves.close()
+
+    #End of program
     ResWindow.mainloop()
--- GenSafety.py
+++ GenSafety.py
@@ -6,35 +6,38 @@ elif version_info.major == 3:
     from tkinter import *
 
 def GSQuiz():
-    cross=[]
+    #Variables
+    cross=[]#Records if the word has been user or not
     lPos=[[],[],[],[]] #letter position
-    dbPos=[[],[]]
-    backgroundcolour = "#460000"
-    choices = ["Yes", "Clean", "Push Stick", "No", "Unplug", "False", "Permission", "Safety Glasses", "Certificate", "Never!"]
-    CheckCorrect = [False,False,False,False,False,False,False,False,False,False]
-    GSWindow = Tk()
-    GSWindow.title("General Safety Quiz")
-    GSWindow.geometry("1000x800")
-    GSWindow.resizable(False,False)
+    dbPos=[[],[]]#Possition of questions and dropdown anchor list
+    backgroundcolour = "#460000" #Variable to set background colour
+    choices = ["Yes", "Clean", "Push Stick", "No", "Unplug", "False", "Permission", "Safety Glasses", "Certificate", "Never!"]#Declaring the possible choices for the user and answers
+    CheckCorrect = [False,False,False,False,False,False,False,False,False,False]#Array to record correct answers
 
-    mycanvas = Canvas(GSWindow, width = 1000, height = 800)
-    mycanvas.create_rectangle(0, 0, 1000, 8000, fill = backgroundcolour)
-    mycanvas.pack(side = "top", fill = "both", expand = True)
+    #Creating tkinter window
+    GSWindow = Tk()#Creating a TK window with alias GSWindow
+    GSWindow.title("General Safety Quiz")#Setting title of window
+    GSWindow.geometry("1000x800")#Making window 1000x800
+    GSWindow.resizable(False,False)#Disabling resizability for x and y directions
+
+    #Creating window canvas
+    mycanvas = Canvas(GSWindow, width = 1000, height = 800)#Creating a canvas ontop of window to enable the extra atributes and precision X,Y placement
+    mycanvas.create_rectangle(0, 0, 1000, 8000, fill = backgroundcolour)#Drawing the background
+    mycanvas.pack(side = "top", fill = "both", expand = True)#Placing the canvas from top to bottom (Cover screen)
 
     #title
-    title=Label(mycanvas,text="General Safety",font=("Helvetica",30),bg=backgroundcolour,fg="white").place(x=500,y=20, anchor= CENTER)
+    title=Label(mycanvas,text="General Safety",font=("Helvetica",30),bg=backgroundcolour,fg="white").place(x=500,y=20, anchor= CENTER)#Add title to windw, middle top of screen
 
     #wordbank
-    xPos=125
-    yPos=100
-    choicePos=[[],[]]
-    side1=mycanvas.create_line(25,75,25,250,width="3",fill="white")
+    xPos=125#Initializing word starting positions
+    yPos=100#See above comment
+    choicePos=[[],[]]#Recording location of words
+    side1=mycanvas.create_line(25,75,25,250,width="3",fill="white")#Creating the 4 borders of wordbank
     side2=mycanvas.create_line(25,250,975,250,width="3",fill="white")
     side3=mycanvas.create_line(975,250,975,75,width="3",fill="white")
     side4=mycanvas.create_line(975,75,25,75,width="3",fill="white")
 
-    #xsPos ysPos xePos yePos
-    #s is start e is end
+    #Mapping strikethrough for wordbank
     for x in range (len(choices)):
         op=mycanvas.create_text(xPos,yPos,text=choices[x],font=('Helvetica', 15),fill="white")
         bbox=mycanvas.bbox(op)
@@ -47,6 +50,7 @@ def GSQuiz():
     for x in range(len(choices)):
         cross.append("")
 
+    #Function for crossing out words in bank
     def strike(selected):
         sIndex=choices.index(selected)
         if(cross[sIndex]==""):
@@ -57,41 +61,42 @@ def GSQuiz():
                 mycanvas.delete(cross[iteration])
                 cross[iteration]=""
 
-
+    #Creating constant positions for questions
     def boxPositioning(x,y,text,bDB):
         space=5
         qxPos=x
         qyPos=y
         q1="Q1: Minor injuries do not need to be reported. -> "
         used=False
-
         for word in text.split(" "):
-
             question=mycanvas.create_text(qxPos,qyPos,text=word, anchor = "nw", font=('Helvetica', 15), fill="white")
             qbox=mycanvas.bbox(question)
             qxPos=qbox[2]+space
-
             if (str(word) == str(bDB) and used==False):
                 dbPos[0]=qbox[2]+space
                 dbPos[1]=qbox[1]
                 used=True
 
+    #----------------------------------
+    #START OF QUESTIONS------
+
     #QUESTION 1---
+    #Placing question one at (25,325) with dropdown anchor '->' !!See boxPositioning function for more detail
     boxPositioning(25,325,"Q1: Minor injuries do not need to be reported. -> ","->")
-    WordChoice1 = StringVar(GSWindow)
-    WordChoice1.set('Answer Here')
-    WordDropdown1 = OptionMenu(mycanvas, WordChoice1, *choices).place(x=dbPos[0],y=dbPos[1])
-    def change_dropdown1(*args):
-        strike(WordChoice1.get())
-        if WordChoice1.get() == "False" or WordChoice1.get() == "No" or WordChoice1.get() == "Never!":
-            CheckCorrect[0] = True
+    WordChoice1 = StringVar(GSWindow)#Declaring a dropdown object
+    WordChoice1.set('Answer Here')#Initializing text when no word has been chosen
+    WordDropdown1 = OptionMenu(mycanvas, WordChoice1, *choices).place(x=dbPos[0],y=dbPos[1])#Placing dropdown object with boxPositioning anchor
+    def change_dropdown1(*args):#Function called when dropdown is changed
+        strike(WordChoice1.get())#Cross out the chosen word from bank !!See strike function for more information
+        if WordChoice1.get() == "False" or WordChoice1.get() == "No" or WordChoice1.get() == "Never!":#Possible correct answers
+            CheckCorrect[0] = True#If answer is correct, record it in list for use in result page
         else:
-            CheckCorrect[0] = False
+            CheckCorrect[0] = False#When incorrect, mark as false
 
-    WordChoice1.trace('w', change_dropdown1)
+    WordChoice1.trace('w', change_dropdown1)#Check dropdown's state
 
 
-    #QUESTION 2---
+    #QUESTION 2--- (See question 1 for comments)
     boxPositioning(25,365,"Q2: If you are uncertain about something in the shop, it is okay to ask a peer. ->","->")
     WordChoice2 = StringVar(GSWindow)
     WordChoice2.set('Answer Here')
@@ -105,7 +110,7 @@ def GSQuiz():
     WordChoice2.trace('w', change_dropdown2)
 
 
-    #QUESTION 3---
+    #QUESTION 3--- (See question 1 for comments)
     boxPositioning(25,405,"Q3: Always get                           from the instructor before using the drill press.","get")
     WordChoice3 = StringVar(GSWindow)
     WordChoice3.set('Answer Here')
@@ -118,7 +123,7 @@ def GSQuiz():
             CheckCorrect[2]=False
     WordChoice3.trace('w', change_dropdown3)
 
-    #QUESTION 4---
+    #QUESTION 4--- (See question 1 for comments)
     boxPositioning(25,445,"Q4: Students are not allowed to use equipment without having a safety                          for that equipment","safety")
     WordChoice4 = StringVar(GSWindow)
     WordChoice4.set('Answer Here')
@@ -131,7 +136,7 @@ def GSQuiz():
             CheckCorrect[3]=False
     WordChoice4.trace('w', change_dropdown4)
 
-    #QUESTION 5---
+    #QUESTION 5--- (See question 1 for comments)
     boxPositioning(25,485,"Q5: Use a                          when cutting small pieces on a bandsaw.","a")
     WordChoice5 = StringVar(GSWindow)
     WordChoice5.set('Answer Here')
@@ -145,7 +150,7 @@ def GSQuiz():
     WordChoice5.trace('w', change_dropdown5)
 
 
-    #QUESTION 6---
+    #QUESTION 6--- (See question 1 for comments)
     boxPositioning(25,525,"Q6: After use,                          and return the tool to its proper place","use,")
     WordChoice6 = StringVar(GSWindow)
     WordChoice6.set('Answer Here')
@@ -159,7 +164,7 @@ def GSQuiz():
     WordChoice6.trace('w', change_dropdown6)
 
 
-    #QUESTION 7---
+    #QUESTION 7--- (See question 1 for comments)
     boxPositioning(25,565,"Q7: When using a machine you should always wear","wear")
     WordChoice7 = StringVar(GSWindow)
     WordChoice7.set('Answer Here')
@@ -173,7 +178,7 @@ def GSQuiz():
     WordChoice7.trace('w', change_dropdown7)
 
 
-    #QUESTION 8---
+    #QUESTION 8--- (See question 1 for comments)
     boxPositioning(25,605,"Q8: It is okay to bring a drink into the shop as long as none of the equipment is running. ->","->")
     WordChoice8 = StringVar(GSWindow)
     WordChoice8.set('Answer Here')
@@ -187,7 +192,7 @@ def GSQuiz():
     WordChoice8.trace('w', change_dropdown8)
 
 
-    #QUESTION 9---
+    #QUESTION 9--- (See question 1 for comments)
     text_canvas = mycanvas.create_text(25, 645, anchor = "nw", font=('Helvetica', 15), fill="white")
     mycanvas.itemconfig(text_canvas, text="Q9: Once you have received your equipment certification you may use the equipment any time without" )
     boxPositioning(25,685," permission. ->","->")
@@ -203,7 +208,7 @@ def GSQuiz():
     WordChoice9.trace('w', change_dropdown9)
 
 
-    #QUESTION 10---
+    #QUESTION 10--- (See question 1 for comments)
     boxPositioning(25,725,"Q10:                          the tool/machine before replacing broken, dull or damaged bits or blades.","Q10:")
     WordChoice10 = StringVar(GSWindow)
     WordChoice10.set('Answer Here')
